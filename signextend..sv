7ef2212958c07f465f95a9719e342cfa97fdb7e8
wmhho <116261192+wmhho@users.noreply.github.com>
2022-11-18 11:50:05 +0000

    Add files via upload

--- signextend..sv
+++ signextend..sv
@@ -1,4 +1,4 @@
-module mux2 #(
+module signextend #(
     parameter   WIDTH = 32,
 )(
     input logic [WIDTH-1:7]     Imm,
@@ -8,13 +8,12 @@ module mux2 #(
 
 always_comb
     case (Immsrc)
-        3'b000: ImmExt = {20{Imm[31]}, Imm[31:20]};
-        3'b001:
-        3'b010:
-        3'b011:
-        3'b100:
-        default:
+        3'b000:     ImmExt = {20{Imm[31]}, Imm[31:20]}; //Immediate
+        3'b001:     ImmExt = {20{Imm[31]}, Imm[31:25], Imm[11:7]} //Store
+        3'b010:     ImmExt = {20{Imm[12]}, Imm[31], Imm[7], Imm[30:25], Imm[11:8]}; //Branch
+        3'b011:     ImmExt = {12{Imm[31]}, Imm[31], Imm[19:12], Imm[20], Imm[30:21]}; //Jump
+        3'b100:     ImmExt = {12{Imm[31]}, Imm[31:12]}; //Upper Immediate
+        default:    ImmExt = {20{Imm[31]}, Imm[31:20]}; //Default is Immediate as we are mainly using it, common case: quickest
     endcase
-
 
 endmodule
