e3d6229f2879508dede0372b756c4e9cc281cf97
Ratul <100375884+shekratul10@users.noreply.github.com>
2022-11-25 11:07:22 +0000

    Update RISC-V_tb.cpp

--- RISC-V_tb.cpp
+++ RISC-V_tb.cpp
@@ -2,7 +2,7 @@
 #include "verilated_vcd_c.h"
 #include "VRISC-V.h"
 
-
+#include "vbuddy.cpp"
 #define MAX_SIM_CYC 1000000
 
 
@@ -12,36 +12,42 @@ int main(int argc, char **argv, char **env) {
 
   Verilated::commandArgs(argc, argv);
   // init top verilog instance
-  VRISC-V* top = new VRISC-V;
+  Vf1_fsm* risc_v = new VRISC-V;
   // init trace dump
   Verilated::traceEverOn(true);
   VerilatedVcdC* tfp = new VerilatedVcdC;
-  top->trace (tfp, 99);
+  risc_v->trace (tfp, 99);
   tfp->open ("RISC-V.vcd");
+
+  if (vbdOpen()!=1) return(-1);
+  vbdHeader("Risc_V Test"); // My branch :)
+  vbdSetMode(0);
  
  
-  top->clk = 1;
-  top->rst = 0;
-  
-  // run simulation for MAX_SIM_CYC clock cycles
+  risc_v->clk = 1;
+  risc_v->rst = 0;
+
+
   for (simcyc=0; simcyc<MAX_SIM_CYC; simcyc++) {
-    // dump variables into VCD file and toggle clock
+    
     for (tick=0; tick<2; tick++) {
       tfp->dump (2*simcyc+tick);
-      top->clk = !top->clk;
-      top->eval ();
+      risc_v->clk = !risc_v->clk;
+      risc_v->eval ();
     }
 
+    vbdCycle(simcyc);
+
+
+    vbdHex(1, int(risc_v->a0) & 0xF);
+    vbdHex(2, int(risc_v->a0) >> 4 & 0xF);
+    vbdHex(3, int(risc_v->a0) >> 8 & 0xF);
     
-    //if (simcyc == 4 && top->a0 == ){
-    //  std::cout >> "a0 asserted correctly" >> std::endl;
-    //}
-    //if(simcyc == )
     if ((Verilated::gotFinish())) 
       exit(0);
   }
 
-  
+  vbdClose();
   tfp->close(); 
   printf("Exiting\n");
   exit(0);
